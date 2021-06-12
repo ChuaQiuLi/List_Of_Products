96b5d01aa6b6fee377aa7e1e5b0f022ce577bb15
20007649 <20007649@rp.edu.sg>
2021-06-13 00:55:43 +0800

    updates

---
+++ app/src/main/java/sg/edu/rp/c346/id20007649/listofproducts/ItemListActivity.java
@@ -0,0 +1,221 @@
+package sg.edu.rp.c346.id20007649.listofproducts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    EditText etProduct;
+    ListView listViewTask;
+    Button btnAdd;
+    Button btnUpdate;
+    Button btnDelete;
+    Spinner spinnerItems;
+    Spinner spinnerFunction;
+    EditText etExpiryDate;
+
+
+    ArrayList<String> listedTask = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+
+        etProduct = findViewById(R.id.etProduct);
+        listViewTask = findViewById(R.id.listViewProduct);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        spinnerItems = findViewById(R.id.spinnerFilter);
+        spinnerFunction = findViewById(R.id.spinnerFunction);
+        etExpiryDate = findViewById(R.id.etExpiryDate);
+
+
+
+        listedTask.add("Expires 2021-11-18 Samsung Galaxy Buds ");
+        listedTask.add("Expires 2021-11-27 Samsung Galaxy A70 ");
+        listedTask.add("Expires 2022-7-31  Samsung Galaxy Flip Z ");
+        listedTask.add("Expires 2021-11-2  Google Pixel 4a ");
+        listedTask.add("Expires 2022-5-6   IPhone 12 series ");
+        listedTask.add("Expires 2022-5-23  Apple Airpods Gen 2 ");
+
+
+
+
+        ArrayAdapter task = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listedTask);
+
+        listViewTask.setAdapter(task);
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String product = "";
+
+                if (listedTask.isEmpty() == false) {
+                    if (etProduct == null ) {
+                        Toast.makeText(ItemListActivity.this,"Please make sure that you have fill up the details before you can add them. ", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    else{
+
+                        product += etProduct.getText().toString();
+                        listedTask.add(product);
+                        task.notifyDataSetChanged();
+                        etProduct.setText(null);
+                        Toast.makeText(ItemListActivity.this,"Product is being added successfully! ", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+
+            }
+        });
+
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int position = Integer.parseInt(etProduct.getText().toString());
+
+
+
+                if (listedTask.isEmpty() == false) {
+                    if (etProduct != null && etExpiryDate != null) {
+                        String product = "";
+
+                        product += etExpiryDate.getText().toString();
+                        listedTask.set(position, product);
+                        task.notifyDataSetChanged();
+                        etProduct.setText(null);
+                        etExpiryDate.setText(null);
+                        Toast.makeText(ItemListActivity.this, "Product is being updated successfully", Toast.LENGTH_LONG).show();
+
+                    }
+
+
+                    else {
+                        if (etExpiryDate == null || etProduct == null) {
+                            Toast.makeText(ItemListActivity.this,"Please make sure that you have fill up the details before you can update them. ", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        else{
+
+                            Toast.makeText(ItemListActivity.this,"Wrong index number ", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+
+
+                }
+
+            }
+        });
+
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String product = "";
+
+
+
+
+                if (listedTask.isEmpty() == true) {
+                    Toast.makeText(ItemListActivity.this,"You don't have any product to remove", Toast.LENGTH_LONG).show();
+                }
+
+                else {
+                    int position = Integer.parseInt(etProduct.getText().toString());
+
+                    if (position >= listedTask.size()) {
+                        Toast.makeText(ItemListActivity.this,"Wrong index number", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    else {
+                        listedTask.remove(position);
+                        task.notifyDataSetChanged();
+                        etProduct.setText(null);
+                        Toast.makeText(ItemListActivity.this,"Product is being deleted Successfully", Toast.LENGTH_LONG).show();
+
+
+                    }
+
+
+                }
+
+            }
+        });
+
+
+        spinnerFunction.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                switch (position) {
+
+                    case 0 :
+
+                        etProduct.setHint("Enter data in this format: Expires <YYYY-M-D> Product Name> ");
+                        etExpiryDate.setEnabled(false);
+                        btnDelete.setEnabled(false);
+                        btnUpdate.setEnabled(false);
+                        btnAdd.setEnabled(true);
+                        break;
+
+
+                    case 1 :
+
+                        etProduct.setHint("Type in the index number  of the product to be updated ");
+                        etExpiryDate.setHint("Write in the following format : Expires <YYYY-M-D> Product Name >");
+                        etExpiryDate.setEnabled(true);
+                        btnAdd.setEnabled(false);
+                        btnUpdate.setEnabled(true);
+                        btnDelete.setEnabled(false);
+                        break;
+
+
+                    case 2 :
+
+                        etProduct.setHint("Type in the index number of the product to be removed");
+                        etExpiryDate.setEnabled(false);
+                        btnAdd.setEnabled(false);
+                        btnUpdate.setEnabled(false);
+                        btnDelete.setEnabled(true);
+                        break;
+
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/sg/edu/rp/c346/id20007649/listofproducts/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20007649/listofproducts/MainActivity.java
@@ -2,13 +2,149 @@ package sg.edu.rp.c346.id20007649.listofproducts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSamsungA70;
+    Button btnSamsungBuds;
+    Button btnAppleAirpods;
+    Button btnIphone12;
+    Button btnGooglePixel;
+    Button btnSamsungFlip;
+    Button btnListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSamsungA70 = findViewById(R.id.btnSamsungA70);
+        btnSamsungFlip = findViewById(R.id.btnFlipZ);
+        btnSamsungBuds = findViewById(R.id.btnGalaxyBuds);
+        btnGooglePixel = findViewById(R.id.btnPixel4a);
+        btnAppleAirpods = findViewById(R.id.btnAirpods);
+        btnIphone12 = findViewById(R.id.btnIphone12);
+        btnListView = findViewById(R.id.btnListView);
+
+
+        btnSamsungA70.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.samsung.com/sg/smartphones/galaxy-a/galaxy-a70-blue-128gb-sm-a705mzbwxsp/"));
+
+                Toast.makeText(MainActivity.this,"Going to Samsung A70 official webpage... ", Toast.LENGTH_LONG).show();
+
+                startActivity(intents);
+
+
+            }
+        });
+
+        btnSamsungFlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.samsung.com/in/smartphones/galaxy-z-flip/"));
+
+                Toast.makeText(MainActivity.this,"Going to Samsung Galaxy Flip Z official webpage... ", Toast.LENGTH_LONG).show();
+
+                startActivity(intents);
+
+
+            }
+        });
+
+
+        btnSamsungBuds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.samsung.com/us/mobile/audio/galaxy-buds/"));
+
+                Toast.makeText(MainActivity.this,"Going to Samsung Galaxy Buds official webpage... ", Toast.LENGTH_LONG).show();
+
+                startActivity(intents);
+
+
+            }
+        });
+
+
+        btnGooglePixel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://store.google.com/sg/product/pixel_4a?utm_source=google&utm_medium=cpc&utm_campaign=japac-SG-en-dr-bkws-all-all-buy-b-dr-1008675&utm_content=text-ad-none-none-DEV_c-CRE_461503521588-ADGP_Hybrid+%7C+BKWS+-+BMM+%7C+Txt+~+Pixel+4a+~+%5BM:1%5D+~+SG+~+en+~+Pixel4a-KWID_43700062296544173-kwd-332343298278-userloc_9062509&utm_term=KW_%2Bpixel+%2B4-ST_%2Bpixel+%2B4&gclid=CjwKCAjwtpGGBhBJEiwAyRZX2hFSziiLw3fOlbG0_NDRbYiQ0HNDEB3xjs-nH1wKf9snCZKha9-CXBoCctgQAvD_BwE&gclsrc=aw.ds&hl=en-GB"));
+
+                Toast.makeText(MainActivity.this,"Going to Google Pixel 4a webpage... ", Toast.LENGTH_LONG).show();
+
+                startActivity(intents);
+
+
+
+            }
+        });
+
+        btnIphone12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.apple.com/sg/iphone-12/?afid=p238%7CsFYUL1Wbu-dc_mtid_20925brv44930_pcrid_517981284702_pgrid_112153639435_&cid=wwa-sg-kwgo-iphone-slid----Avail-"));
+
+                Toast.makeText(MainActivity.this,"Going to IPhone 12 series webpage... ", Toast.LENGTH_LONG).show();
+
+
+                startActivity(intents);
+
+
+            }
+        });
+
+
+        btnAppleAirpods.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intents = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.apple.com/sg/airpods-2nd-generation/"));
+
+                Toast.makeText(MainActivity.this,"Going to Apple Airpods Gen 2 webpage... ", Toast.LENGTH_LONG).show();
+
+
+                startActivity(intents);
+
+
+            }
+        });
+
+
+        btnListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,ItemListActivity.class);
+
+                Toast.makeText(MainActivity.this,"Going to listview of the products that are on sales...", Toast.LENGTH_LONG).show();
+
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
     }
 }
